dd85e1f7d153e1a08d5ad53a62c66410cbdde6a5
David Herrera <herrera.dl@gmail.com>
2011-10-28 12:30:21 -0700

    Incorporate Mark Jaquith's tips for local development

--- README.markdown
+++ README.markdown
@@ -19,7 +19,7 @@ The advantage of keeping `assets` outside of your `wordpress`
 directory is that you can update WordPress more easily. Just overwrite
 the old `wordpress` directory with the new one. There's less worry
 about overwriting in `wordpress/wp-content` because everything should
-be stored in the `assets` folder. That said:
+be stored in the `assets` directory. That said:
 
 1. WordPress will still store file uploads in
 `wordpress/wp-content/uploads` by default. You will need to change
@@ -34,8 +34,30 @@ then delete `wp-config.php`, copy `wp-config-sample.php` from the
 `wp-config.php`, and edit the file normally.
 
 
+## Local development with local-config.php ##
+
+I use the local development configuration suggested by Mark Jaquith.
+This configuration involves creating a separate version of
+`wp-config.php` called `local-config.php`, in which your local
+database values are stored and `WP-DEBUG` is turned on. A conditional
+inside `wp-config.php` checks for the existence of `local-config.php`
+and uses it if found. If not, it continues using the production
+configuration in `wp-config.php`.
+
+When deploying your site to your production server, then, you
+obviously have to ensure that you exclude `local-config.php`, or else
+it will obviously be used. That means that you need to *ignore*
+`local-config.php` after you download the boilerplate, but before you
+configure the local file.
+
+For more information about the configuration, read Mark Jaquith's
+[blog post][3] or watch his [presentation at WordCamp SF 2011][4],
+both of which include other tips for local development.
+
 
 
 
 [1]: http://codex.wordpress.org/Hardening_WordPress#Securing_wp-config.php
 [2]: http://codex.wordpress.org/Editing_wp-config.php#Moving_wp-content
+[3]: http://markjaquith.wordpress.com/2011/06/24/wordpress-local-dev-tips/
+[4]: http://wordpress.tv/2011/08/20/mark-jaquith-scaling-servers-and-deploys-oh-my/
--- wp-config.php
+++ wp-config.php
@@ -14,70 +14,76 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-define('DB_NAME', 'database_name_here');
-define('DB_USER', 'username_here');
-define('DB_PASSWORD', 'password_here');
-define('DB_HOST', 'localhost');
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
 
-// ** Move the wp-content directory ** //
-define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/assets'); // You'll need to add more to this path if your site isn't in root
-define('WP_CONTENT_URL', 'http://[url_here]/assets'); // You definitely need to add the right URL, and maybe add more to the path
+// ** Use the local-config.php file if it exists ** //
+if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
+    require( dirname( __FILE__ ) . '/local-config.php' );
+} else {
+    // ** Move the wp-content directory ** //
+    define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/assets'); // You'll need to add more to this path if your site isn't in root
+    define('WP_CONTENT_URL', 'http://[url_here]/assets'); // You definitely need to add the right URL, and maybe add more to the path
+    // ** MySQL settings - You can get this info from your web host ** //
+    define('DB_NAME', 'database_name_here');
+    define('DB_USER', 'username_here');
+    define('DB_PASSWORD', 'password_here');
+    define('DB_HOST', 'localhost');
 
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+    define('DB_CHARSET', 'utf8');
+    define('DB_COLLATE', '');
 
-/**#@-*/
+    /**#@+
+     * Authentication Unique Keys and Salts.
+     *
+     * Change these to different unique phrases!
+     * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+     * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+     *
+     * @since 2.6.0
+     */
+    define('AUTH_KEY',         'put your unique phrase here');
+    define('SECURE_AUTH_KEY',  'put your unique phrase here');
+    define('LOGGED_IN_KEY',    'put your unique phrase here');
+    define('NONCE_KEY',        'put your unique phrase here');
+    define('AUTH_SALT',        'put your unique phrase here');
+    define('SECURE_AUTH_SALT', 'put your unique phrase here');
+    define('LOGGED_IN_SALT',   'put your unique phrase here');
+    define('NONCE_SALT',       'put your unique phrase here');
 
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix  = 'wp_';
+    /**#@-*/
 
-/**
- * WordPress Localized Language, defaults to English.
- *
- * Change this to localize WordPress. A corresponding MO file for the chosen
- * language must be installed to wp-content/languages. For example, install
- * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
- * language support.
- */
-define('WPLANG', '');
+    /**
+     * WordPress Database Table prefix.
+     *
+     * You can have multiple installations in one database if you give each a unique
+     * prefix. Only numbers, letters, and underscores please!
+     */
+    $table_prefix  = 'wp_';
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
-define('WP_DEBUG', false);
+    /**
+     * WordPress Localized Language, defaults to English.
+     *
+     * Change this to localize WordPress. A corresponding MO file for the chosen
+     * language must be installed to wp-content/languages. For example, install
+     * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+     * language support.
+     */
+    define('WPLANG', '');
 
-/* That's all, stop editing! Happy blogging. */
+    /**
+     * For developers: WordPress debugging mode.
+     *
+     * Change this to true to enable the display of notices during development.
+     * It is strongly recommended that plugin and theme developers use WP_DEBUG
+     * in their development environments.
+     */
+    define('WP_DEBUG', false);
 
-/** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+    /* That's all, stop editing! Happy blogging. */
 
-/** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+    /** Absolute path to the WordPress directory. */
+    if ( !defined('ABSPATH') )
+        define('ABSPATH', dirname(__FILE__) . '/');
+
+    /** Sets up WordPress vars and included files. */
+    require_once(ABSPATH . 'wp-settings.php');
+}
