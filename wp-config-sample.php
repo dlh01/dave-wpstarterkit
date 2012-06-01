b5f73386e600c007171629838e1156fbf6b2e443
David Herrera <herrera.dl@gmail.com>
2012-06-01 11:46:28 -0700

    wp-config.php cleanup

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -1,38 +1,35 @@
 <?php
 /**
  * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
  */
 
-// ** Use the local-config.php file if it exists ** //
+
+/**
+ * Load database info, and use local-config.php if it exists
+ */
 if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
-    define( 'WP_LOCAL_DEV', true ); // For disable-plugins-when-doing-local-dev.php
-    include( dirname( __FILE__ ) . '/local-config.php' );
+  define( 'WP_LOCAL_DEV', true ); // For disable-plugins-when-doing-local-dev.php
+  include( dirname( __FILE__ ) . '/local-config.php' );
 } else {
-    // ** Production MySQL settings - You can get this info from your web host ** //
-    define('DB_NAME', 'database_name_here');
-    define('DB_USER', 'username_here');
-    define('DB_PASSWORD', 'password_here');
-    define('DB_HOST', 'localhost');
-    /* Define the address of the production website and the core WordPress files */
-    define('WP_HOME', 'http://www.example.com');
-    define('WP_SITEURL', 'http://www.example.com/wordpress');
+  define( 'WP_LOCAL_DEV', false );
+
+  /* Production MySQL settings */
+  define('DB_NAME', 'database_name_here');
+  define('DB_USER', 'username_here');
+  define('DB_PASSWORD', 'password_here');
+  define('DB_HOST', 'localhost');
+
+  /* Define the address of the production website and the core WordPress files */
+  define('WP_HOME', 'http://www.example.com');
+  define('WP_SITEURL', 'http://www.example.com/wordpress');
 }
 
+/* You probably don't need to change these */
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
     
-/*
+
+/**
  * Move the wp-content directory
  *
  * You might need to add more to these paths if your site isn't in root. These 
@@ -41,14 +38,11 @@ define('DB_COLLATE', '');
 define('WP_CONTENT_DIR', dirname( __FILE__ ) . '/content');
 define('WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content');
 
-/**#@+
+
+/**
  * Authentication Unique Keys and Salts.
  *
- * Change these to different unique phrases!
  * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
  */
 define('AUTH_KEY',         'put your unique phrase here');
 define('SECURE_AUTH_KEY',  'put your unique phrase here');
@@ -59,44 +53,31 @@ define('SECURE_AUTH_SALT', 'put your unique phrase here');
 define('LOGGED_IN_SALT',   'put your unique phrase here');
 define('NONCE_SALT',       'put your unique phrase here');
 
-/**#@-*/
 
 /**
  * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
  */
 $table_prefix  = 'wp_';
 
+
 /**
  * WordPress Localized Language, defaults to English.
- *
- * Change this to localize WordPress. A corresponding MO file for the chosen
- * language must be installed to wp-content/languages. For example, install
- * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
- * language support.
  */
 define('WPLANG', '');
 
+
 /**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
+ * WordPress debugging mode. This is typically enabled in local-config.php
  */
 // define('WP_DEBUG', false);
 
+
 /**
  * Hide errors
- *
- * via https://github.com/markjaquith/WordPress-Skeleton/blob/master/wp-config.php
  */
 ini_set( 'display_errors', 0 );
 define( 'WP_DEBUG_DISPLAY', false );
 
-/* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
