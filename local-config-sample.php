094e5f57f3c93a31877e424302a3727d0541fb5d
David Herrera <herrera.dl@gmail.com>
2012-06-01 11:39:48 -0700

    Reorganizing the config files
    * Rather than put the entire wp-config in conditionals, only put database settings, HOME, and SITEURL definitions in the conditional
    * Don't define WP_DEBUG as false or else it will override local-config.php
    * Be sure to hide errors on production

--- local-config-sample.php
+++ local-config-sample.php
@@ -3,7 +3,16 @@
  * The base configurations of the local WordPress.
  */
 
+/* MySQL settings */
 define('DB_NAME', 'database_name_here');
 define('DB_USER', 'username_here');
 define('DB_PASSWORD', 'password_here');
 define('DB_HOST', 'localhost');
+
+/* Debugging */
+define( 'SAVEQUERIES', true );
+define( 'WP_DEBUG', true );
+
+/* Define the address of the local website and the core WordPress files */
+define('WP_HOME', 'http://www.example.com');
+define('WP_SITEURL', 'http://www.example.com/wordpress');
--- wp-config-sample.php
+++ wp-config-sample.php
@@ -19,77 +19,89 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
     define( 'WP_LOCAL_DEV', true ); // For disable-plugins-when-doing-local-dev.php
     include( dirname( __FILE__ ) . '/local-config.php' );
 } else {
-    // ** MySQL settings - You can get this info from your web host ** //
+    // ** Production MySQL settings - You can get this info from your web host ** //
     define('DB_NAME', 'database_name_here');
     define('DB_USER', 'username_here');
     define('DB_PASSWORD', 'password_here');
     define('DB_HOST', 'localhost');
+    /* Define the address of the production website and the core WordPress files */
+    define('WP_HOME', 'http://www.example.com');
+    define('WP_SITEURL', 'http://www.example.com/wordpress');
 }
 
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
     
-    // ** Move the wp-content directory ** //
-    define('WP_CONTENT_DIR', dirname( __FILE__ ) . '/content'); // You'll need to add more to this path if your site isn't in root
-    define('WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content'); // You definitely need to add the correct URL, and maybe add more to the path
+/*
+ * Move the wp-content directory
+ *
+ * You might need to add more to these paths if your site isn't in root. These 
+ * paths also assume the same directory structure in local and production installs
+ */
+define('WP_CONTENT_DIR', dirname( __FILE__ ) . '/content');
+define('WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content');
 
-    // ** Define the address of your website and of the core WordPress files ** //
-    define('WP_HOME', 'http://www.example.com'); // Change example.com to the address you want people to type in their browser to reach your site
-    define('WP_SITEURL', 'http://www.example.com/wordpress'); // Change example.com/wordpress to the address where your WordPress core files reside
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
 
-    /**#@+
-     * Authentication Unique Keys and Salts.
-     *
-     * Change these to different unique phrases!
-     * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
-     * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
-     *
-     * @since 2.6.0
-     */
-    define('AUTH_KEY',         'put your unique phrase here');
-    define('SECURE_AUTH_KEY',  'put your unique phrase here');
-    define('LOGGED_IN_KEY',    'put your unique phrase here');
-    define('NONCE_KEY',        'put your unique phrase here');
-    define('AUTH_SALT',        'put your unique phrase here');
-    define('SECURE_AUTH_SALT', 'put your unique phrase here');
-    define('LOGGED_IN_SALT',   'put your unique phrase here');
-    define('NONCE_SALT',       'put your unique phrase here');
+/**#@-*/
 
-    /**#@-*/
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
 
-    /**
-     * WordPress Database Table prefix.
-     *
-     * You can have multiple installations in one database if you give each a unique
-     * prefix. Only numbers, letters, and underscores please!
-     */
-    $table_prefix  = 'wp_';
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define('WPLANG', '');
 
-    /**
-     * WordPress Localized Language, defaults to English.
-     *
-     * Change this to localize WordPress. A corresponding MO file for the chosen
-     * language must be installed to wp-content/languages. For example, install
-     * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
-     * language support.
-     */
-    define('WPLANG', '');
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+// define('WP_DEBUG', false);
 
-    /**
-     * For developers: WordPress debugging mode.
-     *
-     * Change this to true to enable the display of notices during development.
-     * It is strongly recommended that plugin and theme developers use WP_DEBUG
-     * in their development environments.
-     */
-    define('WP_DEBUG', false);
+/**
+ * Hide errors
+ *
+ * via https://github.com/markjaquith/WordPress-Skeleton/blob/master/wp-config.php
+ */
+ini_set( 'display_errors', 0 );
+define( 'WP_DEBUG_DISPLAY', false );
 
-    /* That's all, stop editing! Happy blogging. */
+/* That's all, stop editing! Happy blogging. */
 
-    /** Absolute path to the WordPress directory. */
-    if ( !defined('ABSPATH') )
-        define('ABSPATH', dirname(__FILE__) . '/');
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+  define('ABSPATH', dirname(__FILE__) . '/');
 
-    /** Sets up WordPress vars and included files. */
-    require_once(ABSPATH . 'wp-settings.php');
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
 }
