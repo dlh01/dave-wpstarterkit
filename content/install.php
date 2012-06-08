abe988d3fc265e4c9a7ecd4a944c030c039ba3fc
David Herrera <herrera.dl@gmail.com>
2012-06-08 15:41:06 -0700

    Update install.php documentation

--- content/install.php
+++ content/install.php
@@ -7,6 +7,8 @@
  * wp-admin/includes/upgrade.php. Leaving the function empty causes WordPress 
  * to install without the default post, page, links, categories, etc.
  *
+ * You can delete this file after installing WordPress
+ *
  * @link http://lists.automattic.com/pipermail/wp-hackers/2012-April/042932.html
  * @link http://wpbits.wordpress.com/2007/08/10/automating-wordpress-customizations-the-installphp-way/
  *
