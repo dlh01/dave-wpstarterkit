41362ef5619df0bd8949685f8959b7fca6ef3d85
David Herrera <herrera.dl@gmail.com>
2013-04-26 14:07:30 -0400

    Copy a fresh version of `wp-content` from core into `content`

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ echo -n "
 /**
  * Welcome. This script will:
  *   - Download core to \`./wordpress\`
+ *   - Copy files in \`./wordpress/wp-content/\` to \`./content\`
  *   - Move \`*-config-sample.php\` to \`*-config.php\`
  *   - Remove the Starter Kit Git repository and initialize a new one
  *   - Remove the Starter Kit README
@@ -16,12 +17,13 @@ select result in Yes No; do
 if [[ $result == 'Yes' ]]; then
   echo ""
   wp core download --path=wordpress
+  /bin/cp -r ./wordpress/wp-content/* content
   /bin/mv ./local-config-sample.php ./local-config.php
   /bin/mv ./wp-config-sample.php ./wp-config.php
   /bin/rm -rf ./.git
   git init
   /bin/rm README.markdown
-  /bin/rm init.sh
+  /bin/rm setup.sh
   echo "
 /* Finished setting up. Have fun! */
 "
